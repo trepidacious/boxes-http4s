189d1eec747d0b010e6f8c70a2641346f47d93be
trepidacious <trepidacious@gmail.com>
2016-05-15 21:58:46 +0100

    Use an array to ensure order, and parse out a command and data

--- src/main/scala/org/rebeam/boxes/http4s/BoxIncoming.scala
+++ src/main/scala/org/rebeam/boxes/http4s/BoxIncoming.scala
@@ -24,14 +24,11 @@ import ProductFormats._
 import CollectionFormats._
 import NodeFormats._
 import BasicFormats._
+import TaggedUnionFormats._
 
 import org.rebeam.boxes.persistence.json._
 
 import scala.collection.mutable.ArrayBuffer
-
-sealed trait BoxIncoming {
-  def run[T: Format](t: T): Unit
-}
 
 //This is what we can actually get from an incoming Text, by using a custom parser
 //to pull it apart, first into tokens for the whole incoming json, and then stripping
@@ -39,100 +36,103 @@ sealed trait BoxIncoming {
 //for the token-encoded box contents. We can then use a Replaces[T] to actually read
 //the tokens when the correct box is found, using the magic of typeclasses to know
 //at compile-time that we will use the right type T at run-time.
-case class BoxCommit(revisionIndex: Long, boxId: Long, boxTokens: TokenReader) extends BoxIncoming {
-  def run[T: Format](t: T): Unit = {
-    Shelf.runRepeatedReader(implicitly[Format[T]].replace(t, boxId), boxTokens)    
+
+//The "command" part of each incoming message, which determines what to
+//do with the "data" part. Can be run when provided with a model to run on, 
+//and the data tokens.
+sealed trait BoxIncomingCommand {
+  def revisionIndex: Long
+  def run[M: Format](model: M)(dataTokens: List[Token])
+}
+
+object BoxIncomingCommand {
+  /**
+    * Used to keep a connection alive, indicating the client is still connected.
+    * Carries the revision index since this is useful in tracking where the client
+    * is up to.
+    */
+  case class BoxPing(revisionIndex: Long) extends BoxIncomingCommand {
+    def run[M: Format](model: M)(dataTokens: List[Token]) = {}
+  }
+  /**
+    * Trigger replacement of the contents of Box with given boxId.
+    * The new contents are decoded from the data tokens.
+    */
+  case class BoxReplace(revisionIndex: Long, boxId: Long) extends BoxIncomingCommand {
+    def run[M: Format](model: M)(dataTokens: List[Token]): Unit = {
+      Shelf.runRepeatedReader(implicitly[Format[M]].replace(model, boxId), JsonTokenReader.maximalCasting(BufferTokenReader(dataTokens)))    
+    }
+  }
+  /**
+    * Trigger modification of a data item containing the Box with given boxId.
+    * The modification action is decoded from the data tokens.
+    */
+  case class BoxModify(revisionIndex: Long, boxId: Long) extends BoxIncomingCommand {
+    def run[M: Format](model: M)(dataTokens: List[Token]): Unit = {
+      Shelf.runRepeatedReader(implicitly[Format[M]].modify(model, boxId), JsonTokenReader.maximalCasting(BufferTokenReader(dataTokens)))    
+    }    
   }
 }
 
-case object BoxPing extends BoxIncoming {
-  def run[T: Format](t: T): Unit = {}
+/**
+  * The full contents of an incoming message, including the command, and the
+  * tokens that make up the data that command will use.
+  */
+case class BoxIncoming(command: BoxIncomingCommand, dataTokens: List[Token]) {
+  def run[M: Format](model: M): Unit = command.run(model)(dataTokens)
 }
 
 object BoxIncoming {
-  //This code is awful. I'm sure this can be done recursively, but tokens are 
-  //already mutable, so just do this imperatively for now. Note that this is
-  //at least likely to be reasonably fast, since we just parse the json to
-  //an ArrayBuffer of tokens once, then mess about with that buffer.
-  //TODO make less awful
+  
+  import BoxIncomingCommand._
+  
+  //Format for each BoxIncomingCommand type, then for BoxIncomingMessage itself
+  implicit val boxIncomingCommandFormat = {
+    implicit val boxPingFormat = productFormat1(BoxPing.apply)("revisionIndex")
+    implicit val boxReplaceFormat = productFormat2(BoxReplace.apply)("revisionIndex", "boxId")
+    implicit val boxModifyFormat = productFormat2(BoxModify.apply)("revisionIndex", "boxId")
+    
+    taggedUnionFormat[BoxIncomingCommand](
+      {
+        case "ping" => boxPingFormat
+        case "replace" => boxReplaceFormat
+        case "modify" => boxModifyFormat
+      },
+      {
+        case p: BoxPing => Tagged("ping", p)
+        case r: BoxReplace => Tagged("replace", r)
+        case m: BoxModify => Tagged("modify", m)
+      }
+    )
+  }
+
+  //FIXME: This is a bit messy.
   @throws [IncorrectTokenException]
   def apply(s: String): BoxIncoming = {
     val tokens = JsonIO.arrayBufferFromJsonString(s)
     
-    if (tokens.size < 3) throw new IncorrectTokenException("< 2 tokens at start")  
-    if (!isOpenDict(tokens.remove(0))) throw new IncorrectTokenException("first token not OpenDict")  
+    //We expect at least open array, close array, end token
+    if (tokens.size < 3) throw new IncorrectTokenException("< 3 tokens at start")  
+    if (!isOpenArr(tokens.remove(0))) throw new IncorrectTokenException("first token not OpenArray")  
     if (tokens.remove(tokens.size - 1) != EndToken) throw new IncorrectTokenException("last token not EndToken")  
-    if (tokens.remove(tokens.size - 1) != CloseDict) throw new IncorrectTokenException("penultimate token not CloseDict")  
+    if (tokens.remove(tokens.size - 1) != CloseArr) throw new IncorrectTokenException("penultimate token not CloseArray")  
 
-    //If we are now empty, we have just a ping
-    if (tokens.isEmpty) {
-      BoxPing
-    } else {
+    //The tokens now denote the command, then the data for the command.
+    //First pull out the command
+    val readerBuffer = BufferTokenReader(tokens.toList)
+    val reader = JsonTokenReader.maximalCasting(readerBuffer)
+    val command = Shelf.runRepeatedReader(boxIncomingCommandFormat.read, reader)._2
+    
+    //We now just have the tokens for the data left in the reader buffer
+    val dataTokens = readerBuffer.remainingTokens
 
-      //We will fill these in as we find them
-      var boxId = none[Long]
-      var revisionIndex = none[Long]
-      
-      //We should now just have the dict entries for revisionIndex, boxId and 
-      //boxContents. We can keep getting the first dict entry as long as it is
-      //not the contents - when we get to them we need to stop
-      var boxContentsFound = false
-      while (!boxContentsFound) {
-        if (tokens.size < 2) throw new IncorrectTokenException("< 2 tokens when requiring more fields")
-        var name = dictEntryNameOption(tokens.remove(0))
-        if (name.isEmpty) throw new IncorrectTokenException("token not dict entry as expected")
-        if (name == Some("boxId")) {
-          boxId = some(JsonMaximalCasting.toLong(tokens.remove(0)))
-        } else if (name == Some("revisionIndex")) {
-          revisionIndex = some(JsonMaximalCasting.toLong(tokens.remove(0)))
-        } else if (name == Some("boxContents")) {
-          boxContentsFound = true;
-        } else {
-          throw new IncorrectTokenException("dict entry for unwanted key " + name)
-        }
-      }
-      
-      //While we have not yet found boxId and revisionIndex, work back from the
-      //end of the tokens
-      while (boxId.isEmpty || revisionIndex.isEmpty) {
-        if (tokens.size < 2) throw new IncorrectTokenException("< 2 tokens when requiring more fields")
-
-        //The last token must be a long value for one or the other of the fields
-        val l = JsonMaximalCasting.toInt(tokens.remove(tokens.size - 1))
-        
-        //Now the last token must be the dict entry - find out which field we got!
-        var name = dictEntryNameOption(tokens.remove(tokens.size - 1))
-        if (name.isEmpty) throw new IncorrectTokenException("token not dict entry as expected")
-        if (name == Some("boxId")) {
-          boxId = Some(l)
-        } else if (name == Some("revisionIndex")) {
-          revisionIndex = Some(l)
-        } else {
-          throw new IncorrectTokenException("dict entry for unwanted key " + name)
-        }
-      }
-      
-      //We should now have everything, and some tokens left for the reader...
-      if (tokens.size > 0) {
-        //Note we wrap the BufferTokenReader in a JsonTokenReader to apply
-        //appropriate casting to the tokens when using e.g. pullLong, pullFloat etc.
-        //We use "maximal" casting to tolerate numbers as strings.
-        val commitOption = (revisionIndex |@| boxId){ BoxCommit(_, _, JsonTokenReader.maximalCasting(BufferTokenReader(tokens.toList))) }
-        commitOption.getOrElse(throw new IncorrectTokenException("invalid contents"))
-      } else {
-        throw new IncorrectTokenException("no tokens left for box contents")        
-      }
-    }
+    //Here's our incoming message
+    BoxIncoming(command, dataTokens)
   }
   
-  private def isOpenDict(t: Token): Boolean = t match {
-    case OpenDict(_, _) => true
+  private def isOpenArr(t: Token): Boolean = t match {
+    case OpenArr(_) => true
     case _ => false
-  }
-  
-  private def dictEntryNameOption(t: Token): Option[String] = t match {
-    case DictEntry(key, _) => Some(key)
-    case _ => None
   }
   
 }
